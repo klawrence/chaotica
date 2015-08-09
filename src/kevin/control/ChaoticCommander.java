54c087c5372e73a35c25323e348342e91b1e11bc
kevin <kevin@smartpatients.com>
2015-08-09 01:09:06 -0700

    sort by target value

--- src/kevin/control/ChaoticCommander.java
+++ src/kevin/control/ChaoticCommander.java
@@ -35,7 +35,7 @@ public class ChaoticCommander extends Commander {
     //    TODO run away from strong robots if(hitMe > hits)
 
     public void fight() {
-        sequence = robot.getTime() % 120;
+        sequence = robot.getTime() % 100;
 
         attack();
         scan();
@@ -50,7 +50,7 @@ public class ChaoticCommander extends Commander {
                 robot.setGunColor(Color.magenta);
                 driver.ram(target);
             }
-            else if (sequence < 80 && robot.getTime() > 50 && robot.getEnergy() > 2) {
+            else if (sequence > 30 && robot.getEnergy() > 2) {
                 robot.setGunColor(gunColor);
                 driver.headTowards(target);
             }
--- src/kevin/control/Enemy.java
+++ src/kevin/control/Enemy.java
@@ -84,7 +84,7 @@ public class Enemy {
     }
 
     public String toString() {
-        return String.format("%-30s: %d\t%d\t%d", name, (int) distance, stats.targetValue(), hitRate());
+        return String.format("%-30s: \t%d", name, stats.targetValue());
     }
 
     public void increasePowerToHitBy(double delta) {
