1169052ba3ff408e96f7348bab319337d8698699
kevin <kevin@smartpatients.com>
2015-08-09 16:13:02 -0700

    TWEAKING

--- src/kevin/control/ChaoticCommander.java
+++ src/kevin/control/ChaoticCommander.java
@@ -45,7 +45,7 @@ public class ChaoticCommander extends Commander {
                 robot.setGunColor(Color.magenta);
                 driver.ram(target);
             }
-            else if (sequence > 50 && robot.getEnergy() > 10) {
+            else if (sequence > 30 && robot.getEnergy() > 10) {
                 robot.setGunColor(gunColor);
                 driver.headTowards(target);
             }
